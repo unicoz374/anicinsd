8d42d664c0a744c06d3c545bf40419d58792c982
unicoz374 <banserricardo670@gmail.com>
2026-09-11 11:24:15 +0700

    Update supabaseClient.js

--- anicine/lib/supabaseClient.js
+++ anicine/lib/supabaseClient.js
@@ -1,7 +1,7 @@
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
-const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
+const supabaseUrl = process.env.sb_publishable_HIDlgXB2KaGOK0ZY8OozWw_pw24W1t8;
+const supabaseAnonKey = process.env.eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InljZ2xjcWFxZWx2bWhjeW1qdnF2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODkwMjMzMzAsImV4cCI6MjEwNDU5OTMzMH0.QBSYE4idV38K14t166VI0Y4tpm6kufdhnJVZL4cvXys;
 
 // Client ini dipakai di sisi browser (register, login, ambil data yang boleh diakses user biasa).
 // Aturan akses sesungguhnya tetap dijaga oleh Row Level Security (RLS) di Supabase,
